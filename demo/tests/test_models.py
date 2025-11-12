f241def8aeda4f4174d933d54433c7d0d56c522b
Bommer5 <nathan23.brico@gmail.com>
2025-11-12 15:34:33 +0100

    [FIX] #chomeurssss

--- demo/tests/test_models.py
+++ demo/tests/test_models.py
@@ -29,7 +29,8 @@ class TestUsers(TransactionCase):
                     'name': 'Marie-Noël',
                     'login': 'mnv',
                 })
-                user.description = None
+        self.assertEqual(user.description, None)
+
 
 
     def test_description_one_line(self):
